8b5eac301011bec596bb315f83c56e75500d3df0
esalamanca06 <erick_salamanca@icloud.com>
2024-04-09 21:45:31 -0500

    Modificación sobre entorno visual en el header y boton leer más

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ export const Header = () => {
           {/* <p className="infoTitlemin bounceIn">En las tecnologias</p> */}
           <p className="infoDesc">Somos esa empresa que necesitas en tu ámbito laboral, te ayudamos a hacer crecer el conocimiento técnico y estratégico de tu equipo enfocándolos y formándolos en las herramientas que actualmente generan más demanda de mercado a nivel de desarrollo e implementación de optimizaciones enfocadas en la optimización de procesos.</p>
           <div className="btnMore">
-            <a className="btn" href="/more">Leer más</a>
+           {/*  <a className="btn" href="/more">Leer más</a> */}
           </div>
         </div>
       </section>
